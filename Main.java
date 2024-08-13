b712b995d8799e442c3880fec76320a1da79fd65
Guilherme Mormito <133173348+Mormito@users.noreply.github.com>
2024-08-12 22:00:54 -0300

    final-update

--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ public class Main {
         Btree a = new Btree();
 
         //add values in the tree
-        a.add(42);
-        a.add(19);
-        a.add(51);
-        a.add(37);
-        a.add(23);
-        a.add(42);
-        a.add(47);
-        a.add(91);
-        a.add(56);
-        a.add(27);
-        a.add(39);
-        a.add(46);
-        a.add(32);
+        a.add(100);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.add(6);
+        a.add(7);
+        a.add(8);
+        a.add(9);
+        a.add(99);
         a.add(10);
+        a.add(10);
+        a.add(22);
+        a.add(23);
 
         //show the numbers
         a.show();
